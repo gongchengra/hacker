432bbea418fb31cbf2b4e0328d49536e1f548fa5
gongchengra <gongchengra@gmail.com>
2014-01-12 16:14:01 +0800

    Update lib_cal.js

--- js/lib_cal.js
+++ js/lib_cal.js
@@ -34,3 +34,47 @@ var sumOfLargeNumbers = function sumOfLargeNumbers(inputArray) {
     return resultArray.join("");
 }
 
+var productOfLargeNumbers = function productOfLargeNumbers(multiplier, multiplicand ) {
+    var multiplierArray = multiplier.toString().split("");
+    var multiplicandArray  = multiplicand.toString().split("");
+    var lenMultiplier = multiplierArray.length;
+    var lenMultiplicand = multiplicandArray.length;
+    var resultArray = [];
+    for(var i = 0; i < lenMultiplier; i++){
+        for(var j= 0; j < lenMultiplicand; j++){
+            if(resultArray[i+j] === undefined) {
+                resultArray[i+j] = 0;
+            }
+            resultArray[i+j] += multiplierArray[i]*multiplicandArray[j];
+        }
+    }
+    for(var k = resultArray.length; k > 0; k--) {
+        if(resultArray[k]>9) {
+            resultArray[k-1] += parseInt(resultArray[k]/10);
+            resultArray[k] = resultArray[k]%10;
+        }
+    }
+    resultArray0 = resultArray[0];
+    if(resultArray0 > 9) {
+        resultArray[0] %= 10;
+        while(resultArray0 !== 0) {
+            resultArray.unshift(resultArray0%10);
+            resultArray0 = parseInt(resultArray0/10);
+        }
+    }
+    return resultArray.join("");
+}
+
+var factorialOfN = function factorialOfN (N) {
+    var intN = parseInt(N);
+    if(intN <= 1) {
+        return 1;
+    } else {
+        var product = 1;
+        for(var i=2; i<= intN; i++) {
+            product *= i;
+        }
+        return product;
+    }
+}
+
