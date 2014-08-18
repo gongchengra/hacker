9f829c2810f1c9872348355c64c36557ae0a75a9
Cheng Gong <gongchengra@gmail.com>
2014-08-18 18:19:29 +0800

    add external variable

--- c/the_c_language_programming/eg_01_08_arg.c
+++ c/the_c_language_programming/eg_01_08_arg.c
@@ -10,10 +10,10 @@ int power(int base, int n)
 }
 
 /* test power function */
-main()
+int main()
 {
     int i;
-    for (i = 0; i < 10; ++i)
+    for (i = 0; i < 20; ++i)
         printf("%d %d %d\n", i, power(2,i), power(-3,i));
     return 0;
 }
---
+++ c/the_c_language_programming/eg_01_10_external_variable.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* maximum input line size */
+int max;
+char line[MAXLINE];
+char longest[MAXLINE]; /* maximum length seen so far */
+/* current input line */
+/* longest line saved here */
+int getlinekr(void);
+void copy(void);
+/* print longest input line; specialized version */
+int main()
+{
+    int len;
+    extern int max;
+    extern char longest[];
+    max = 0;
+    while ((len = getlinekr()) > 0)
+        if (len > max) {
+            max = len;
+            copy();
+        }
+    if (max > 0) /* there was a line */
+        printf("%s", longest);
+    return 0;
+}
+/* getline: specialized version */
+int getlinekr(void)
+{
+    int c, i;
+    extern char line[];
+    for (i = 0; i < MAXLINE - 1 && (c=getchar()) != EOF && c != '\n'; ++i)
+                line[i] = c;
+    if (c == '\n') {
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+    return i;
+}
+/* copy: specialized version */
+void copy(void)
+{
+    int i;
+    extern char line[], longest[];
+    i = 0;
+    while ((longest[i] = line[i]) != '\0')
+        ++i;
+}
