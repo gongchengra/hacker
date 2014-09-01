fc182863851866c11156c52d56173f76ad3a537d
Cheng Gong <gongchengra@gmail.com>
2014-09-01 14:35:22 +0800

    add description in projecteuler

--- js/projecteuler004.js
+++ js/projecteuler004.js
@@ -1,6 +1,7 @@
 //Author: gongchengra@gmail.com
 //Solve the problem in http://projecteuler.net/problem=4
-//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99.
+//A palindromic number reads the same both ways. 
+//The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99.
 //
 //Find the largest palindrome made from the product of two 3-digit numbers.
 var isPalindromeNumber = function isPalindromeNumber(N) {
