1bbf3c96a92ae1728bb0089d32490f4fcaf3d6ec
Cheng Gong <gongchengra@gmail.com>
2014-11-05 14:03:57 +0800

    add get N prime function

--- c/03_get_N_prime_number.c
+++ c/03_get_N_prime_number.c
@@ -22,14 +22,41 @@ int *getPrimeNumbersLessthanN(int N, int *resultArray) {
     return 0;
 }
 
+int *getNPrimeNumbers(int N, int *resultArray) {
+    //According to Prime_number_theorem,
+    //the N prime number is approximately N*ln(N),
+    //in order to make sure we can get N numbers, we multiply by a factor 1.2
+    unsigned long tmp;
+    if(N < 1) {
+        return 0;
+    } else if (N < 20) {
+        getPrimeNumbersLessthanN(3*log(N)*N, resultArray);
+    } else {
+        getPrimeNumbersLessthanN(1.2*log(N)*N, resultArray);
+    }
+    for(tmp = N; tmp < (sizeof(resultArray)/sizeof(int)); tmp++){
+        resultArray[tmp] = 0;
+    }
+    return 0;
+}
+
 int main(void)
 {
     unsigned int i;
     int primes[Numbers]={0};
+    printf("The prime numbers less than 1000000 are listed: \n");
     getPrimeNumbersLessthanN(1000000, primes);
     for(i = 0; primes[i] != 0 && i < (sizeof(primes)/sizeof(int)); i++){
         printf("%7d", primes[i]);
         printf((i%10 == 9)?"\n":" ");
+        primes[i] = 0;
     }
+    printf("\nThe first 1000 prime numbers are listed: \n");
+    getNPrimeNumbers(5, primes);
+    for(i = 0; primes[i] != 0 && i < (sizeof(primes)/sizeof(int)); i++){
+        printf("%7d", primes[i]);
+        printf((i%10 == 9)?"\n":" ");
+    }
+    printf("\n");
     return 0;
 }
