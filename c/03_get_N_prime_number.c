63674f006d8089f67069378c3946eca6e7b45082
Cheng Gong <gongchengra@gmail.com>
2014-11-11 18:02:09 +0800

    change prime range

--- c/03_get_N_prime_number.c
+++ c/03_get_N_prime_number.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define Numbers 1000000
+#define Numbers 5000000
 
-int *getPrimeNumbersLessthanN(int N, int *resultArray) {
+int getPrimeNumbersLessthanN(int N, int *resultArray) {
     char primeArray[Numbers]={0};
     int i, j;
     for (i = 2; i <= sqrt(N); i++) {
@@ -22,20 +22,19 @@ int *getPrimeNumbersLessthanN(int N, int *resultArray) {
     return 0;
 }
 
-int *getNPrimeNumbers(int N, int *resultArray) {
+int getNPrimeNumbers(int N, int *resultArray) {
     //According to Prime_number_theorem,
     //the N prime number is approximately N*ln(N),
     //in order to make sure we can get N numbers, we multiply by a factor 1.2
-    unsigned long tmp;
+    int tmp, max;
     if(N < 1) {
         return 0;
-    } else if (N < 20) {
-        getPrimeNumbersLessthanN(3*log(N)*N, resultArray);
     } else {
-        getPrimeNumbersLessthanN(1.2*log(N)*N, resultArray);
-    }
-    for(tmp = N; tmp < (sizeof(resultArray)/sizeof(int)); tmp++){
-        resultArray[tmp] = 0;
+        max = 3*log(N)*N;
+        getPrimeNumbersLessthanN(max, resultArray);
+        for(tmp = max; tmp >= N; tmp--){
+            resultArray[tmp] = 0;
+        }
     }
     return 0;
 }
@@ -45,14 +44,14 @@ int main(void)
     unsigned int i;
     int primes[Numbers]={0};
     printf("The prime numbers less than 1000000 are listed: \n");
-    getPrimeNumbersLessthanN(1000000, primes);
+    getPrimeNumbersLessthanN(5000000, primes);
     for(i = 0; primes[i] != 0 && i < (sizeof(primes)/sizeof(int)); i++){
         printf("%7d", primes[i]);
         printf((i%10 == 9)?"\n":" ");
         primes[i] = 0;
     }
-    printf("\nThe first 1000 prime numbers are listed: \n");
-    getNPrimeNumbers(5, primes);
+    printf("\nThe first 10 prime numbers are listed: \n");
+    getNPrimeNumbers(10, primes);
     for(i = 0; primes[i] != 0 && i < (sizeof(primes)/sizeof(int)); i++){
         printf("%7d", primes[i]);
         printf((i%10 == 9)?"\n":" ");
