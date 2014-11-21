063677d40076bffcf99d7fa788a46b4e529cfc7f
Cheng Gong <gongchengra@gmail.com>
2014-11-21 19:42:44 +0800

    modify the struct code

--- c/the_c_language_programming/eg_06_01_struct.c
+++ c/the_c_language_programming/eg_06_01_struct.c
@@ -23,18 +23,12 @@ int main(void)
     struct rect {
         struct point pt1;
         struct point pt2;
-    };
+    } screen;
     double dist;
-    rect screen;
     pt.x = 320;
     pt.y = 300;
     printf("%d, %d\n", pt.x, pt.y);
     dist = sqrt((double)pt.x * pt.x + (double)pt.y * pt.y);
     printf("%f\n", dist);
-    screen.pt1.x = 0;
-    screen.pt1.y = 0;
-    screen.pt2.x = 1920;
-    screen.pt2.y = 1080;
-
     return 0;
 }
