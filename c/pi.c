70279a1828180f875911b696d5eb4246497c7d61
Cheng Gong <gongchengra@gmail.com>
2015-01-08 13:57:27 +0800

    changed pi.c

--- c/pi.c
+++ c/pi.c
@@ -1,72 +1,158 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 
-* File Name : pi.c
+ * File Name : pi.c
 
-* Author : gongchengra@gmail.com
+ * Author : gongchengra@gmail.com
 
-* Purpose : Calculate pi
-*
-* pi=16actan(1/5)-4actan(1/239)
-*
-* actan（x）=x-x**3/3+x**5/5-x**7/7+....
-*
-* pi=3.2-4/239-1/3*(3.2/25-4/239**3)+1/5*(3.2/25**2-4/239**5)+...
-*
-* use d5 to store the 16/5, 16/5**3, 16/5**5, ...
-*
-* use d239 to store the divide of 4/239, 4/239**3, 4/239**5, ...
-*
-* use m239 to store the modulo of 4/239, 4/239**3, 4/239**5, ...
-*
-* use dpi to store the divide of (16/5-4/239), (16/5-4/239)/3, (16/5-4/239)/5, ...
-*
-* use mpi to store the modulo of (16/5-4/239), (16/5-4/239)/3, (16/5-4/239)/5, ...
-*
-* use pi to store the final result
+ * Purpose : Calculate pi
+ *
+ * pi=16actan(1/5)-4actan(1/239)
+ *
+ * actan（x）=x-x**3/3+x**5/5-x**7/7+....
+ *
+ * pi=3.2-4/239-1/3*(3.2/25-4/239**3)+1/5*(3.2/25**2-4/239**5)+...
+ *
+ * use d5 to store 16/5, 16/5**3, 16/5**5, ...
+ *
+ * use d239 to store 4/239, 4/239**3, 4/239**5, ...
+ *
+ * use t5 to store 16/5, 1/3*(16/5**3), 1/5*(16/5**5), ...
+ *
+ * use t239 to store 4/239, 1/3*(4/239**3), 1/5*(4/239**5), ...
+ *
+ * use pi to store the final result
 
-* Creation Date : 2015-01-07
+ * Creation Date : 2015-01-07
 
-* Last Modified :
+ * Last Modified :
 
-_._._._._._._._._._._._._._._._._._._._._.*/
+ _._._._._._._._._._._._._._._._._._._._._.*/
 
 #include <stdio.h>
 #define NUMBER 10000
+int array_divide_number(int *array, int number, int size)
+{
+    int i,tmp;
+    int modulo=0;
+    for(i=0;i<size;i++)
+    {
+        tmp = array[i]+modulo*10;
+        array[i] = tmp/number;
+        modulo = tmp%number;
+    }
+    return 0;
+}
+
+void print_array(int *array, int size)
+{
+    int i,last;
+    for(last=size-1;last>=0;last--)
+    {
+        if(array[last] != 0)
+        {
+            break;
+        }
+    }
+    for(i=0;i<=last;i++)
+    {
+        printf("%d",array[i]);
+    }
+}
+
+void copy_array(int *source, int *target, int size)
+{
+    int i;
+    for(i=0;i<size;i++)
+    {
+        target[i] = source[i];
+    }
+}
+
+void plus_array(int *augend, int *addend, int *sum, int size)
+{
+    int i;
+    for(i=0;i<size;i++)
+    {
+        sum[i] = augend[i] + addend[i];
+    }
+    for(i=size-1;i>0;i--)
+    {
+        if(sum[i]>9)
+        {
+            sum[i] = sum[i] % 10;
+            sum[i-1] += sum[i] / 10;
+        }
+    }
+}
+
+void minus_array(int *minuend, int *subtracter, int *answer, int size)
+{
+    int i;
+    for(i=0;i<size;i++)
+    {
+        answer[i] = minuend[i] - subtracter[i];
+    }
+    for(i=size-1;i>0;i--)
+    {
+        if(answer[i]<0)
+        {
+            if(answer[i-1] == 0)
+            {
+                answer[i-2]--;
+                answer[i-1]=10;
+            }
+            answer[i-1]--;
+            answer[i] = 10 + answer[i];
+        }
+    }
+}
+
 int main()
 {
-    int i,j,k,tmp;
+    int i;
     int flag=1;
     int d5[NUMBER]={0};
     int t5[NUMBER]={0};
     int d239[NUMBER]={0};
     int t239[NUMBER]={0};
-    int m239[NUMBER]={0};
+    int pt[NUMBER]={0};
     int pi[NUMBER]={0};
 
-    //  16/5=3.2;
-    t5[0]=3;
-    t5[1]=2;
-    t239[0]=4;
-    for(j=1;j<NUMBER;j++)
-    {
-        d239[j]=t239[j];
-        tmp=d239[j-1]*10+d239[j];
-        t239[j]=tmp/239;
-        m239[j]=tmp%239;
-    }
+    d5[0]=16;
+    d239[0]=4;
+    array_divide_number(d5,5,NUMBER);
+    array_divide_number(d239,239,NUMBER);
 
-    for(j=0;j<NUMBER;j++)
-    {
-        pi[j]+=t5[j];
-    }
-    for(j=NUMBER-1;j>=0;j--)
-    {
+/*    copy_array(pi, pt, NUMBER);*/
+/*    plus_array(pi,d5,pi,NUMBER);*/
+/*    print_array(pi, NUMBER);*/
+/*    printf("\n");*/
+/*    copy_array(pi, pt, NUMBER);*/
+/*    minus_array(pi,d239,pi,NUMBER);*/
+        for(i=1;i<NUMBER/4;i+=2)
+        {
+            copy_array(d5, t5, NUMBER);
+            copy_array(d239, t239, NUMBER);
 
-    }
+            array_divide_number(t5,i,NUMBER);
+            array_divide_number(t239,i,NUMBER);
 
-    for(i=1;i<NUMBER/4;i++)
-    {
+            if(flag > 0)
+            {
+                plus_array(pi,t5,pi,NUMBER);
+                minus_array(pi,t239,pi,NUMBER);
+            }
+            else
+            {
+                minus_array(pi,t5,pi,NUMBER);
+                plus_array(pi,t239,pi,NUMBER);
+            }
+            flag = -1*flag;
 
-    }
+            array_divide_number(d5,5*5,NUMBER);
+            array_divide_number(d239,239*239,NUMBER);
+        }
 
+    print_array(pi, NUMBER);
+    return 0;
 }
