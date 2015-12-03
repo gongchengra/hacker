0dda135604f90e29475ce36fa2f5b59ff9c5eeab
Alan <gongchengra@gmail.com>
2015-12-03 16:14:12 +0800

    update ufw rules

--- bash/ufw.sh
+++ bash/ufw.sh
@@ -1,6 +1,10 @@
-sudo ufw default deny
+#!/bin/bash
+#sudo ufw default deny
+sudo ufw allow ssh
 sudo ufw allow http
 sudo ufw allow https
-sudo ufw allow ssh
 sudo ufw allow git
+sudo ufw allow mysql
+sudo ufw allow rsync
+sudo ufw allow 5501
 sudo ufw enable
