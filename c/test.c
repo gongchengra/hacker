9ddbf1a53abcf420a2dbdbe62353b1ea0a0c6395
龚成 <gongcheng749@pingan.com.cn>
2019-10-10 09:22:03 +0800

    test snipmate

---
+++ c/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// main<tab>
+int main(int argc, const char *argv[])
+{
+    int sum = 0;
+    // for<tab>
+    for (int i = 0; i < 50; i++) {
+        sum += i; 
+    }
+    printf("%d", sum);
+    return 0;
+}
